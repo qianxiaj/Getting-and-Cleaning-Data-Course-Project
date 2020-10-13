8106819dbaa627594f03ca41aa4d197b829d01f9
Qianxia <qianxia.jiang@uconn.edu>
2020-10-12 22:43:14 -0700

    run_analysis

---
+++ run_analysis.R
@@ -0,0 +1,32 @@
+#Step1: Merges the datasets
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+Merged_Data <- cbind(Subject, Y, X)
+#Step2: Get only the measurements on the mean & SD 
+TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+#Step3: name the activities
+TidyData$code <- activities[TidyData$code, 2]
+#Step4: labels the dataset
+names(TidyData)[2] = "activity"
+names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
+names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData)<-gsub("^t", "Time", names(TidyData))
+names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
+names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("angle", "Angle", names(TidyData))
+names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+#Step5:create a independent tidy dataset with the average of each variable for each activity and subject.
+FinalData <- TidyData %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(FinalData, "FinalData.txt", row.name=FALSE)
+#check each variable name
+str(FinalData)
+#look at final data
+FinalData
